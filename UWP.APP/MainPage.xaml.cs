75f82b1aa3de388d5fa3eeea1e514a472d96442a
kanekotic <alvarojosepl@gmail.com>
2016-07-27 07:28:31 +0200

    added leaking code

--- UWP.APP/MainPage.xaml.cs
+++ UWP.APP/MainPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using UWP.LIB.MemoryLeaks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -17,15 +19,36 @@ using Windows.UI.Xaml.Navigation;
 
 namespace UWP.APP
 {
+    
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public event EventHandler DoLeak;
         public MainPage()
         {
             DataContext = new MainPageViewModel();
             this.InitializeComponent();
+            //Memory leak
+            /*Task.Run(() =>
+            {
+                LeakEvent ml;
+                while (true)
+                {
+                    ml = new LeakEvent();
+                    DoLeak += ml.DoLeak;
+                }
+            });*/
+            //No memory Leak
+            /*Task.Run(() =>
+            {
+                NoLeakEvent ml;
+                while (true)
+                {
+                    ml = new NoLeakEvent((object obj, EventArgs args) => { });
+                }
+            });*/
         }
     }
 }
---
+++ UWP.LIB/MemoryLeaks/LeakEvent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP.LIB.MemoryLeaks
+{
+    public class LeakEvent
+    {
+        string value = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+        public LeakEvent()
+        {
+        }
+
+        public void DoLeak(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+    public class NoLeakEvent
+    {
+        string value = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
+        public event EventHandler DontDoLeak;
+        public NoLeakEvent(EventHandler handler)
+        {
+            DontDoLeak += handler;
+        }
+
+    }
+}
